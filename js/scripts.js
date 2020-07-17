9b0f35baccd10a797cdc4a463b42d20b2ee4ad4f
Silvia Eveline <evelineborges1992@gmail.com>
2020-07-17 00:04:16 -0300

    Parte 03

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ var globalNames = ["Um", "Dois", "Tres", "Quatro"];
 var inputName = null;
 
 function start() {
-    inputName = document.querySelector("#inputName");
+  inputName = document.querySelector("#inputName");
 
   preventFormSubmit();
   activateInput();
@@ -23,6 +23,7 @@ function preventFormSubmit() {
 function activateInput() {
   function insertName(newName) {
     globalNames.push(newName);
+    render();
   }
 
   function handleTyping(event) {
@@ -36,17 +37,45 @@ function activateInput() {
 }
 
 function render() {
+  function createDeleteButton(index) {
+      function deleteName(){
+          globalNames.splice(index, 1);
+          render();
+      }
+    var button = document.createElement("button");
+    button.classList.add("deleteButton");
+    button.textContent = "x";
+
+button.addEventListener('click', deleteName);
+
+    return button;
+  }
+
   var divNames = document.querySelector("#names");
+  divNames.innerHTML = "";
 
   var ul = document.createElement("ul");
-  for (i = 0; i < globalNames.length; i++) {
-      var currentName = globalNames[i];
 
-      var li
-       = document.createElement('li');
-       li.textContent = currentName;
-       ul.appendChild(li);
+  for (i = 0; i < globalNames.length; i++) {
+    var currentName = globalNames[i];
+
+    var li = document.createElement("li");
+    var button = createDeleteButton(i);
+
+    var span = document.createElement("span");
+    span.textContent = currentName;
+
+    li.appendChild(button);
+    li.appendChild(span);
+    
+    ul.appendChild(li);
   }
 
   divNames.appendChild(ul);
+  clearInput();
+}
+
+function clearInput() {
+  inputName.value = "";
+  inputName.focus();
 }
